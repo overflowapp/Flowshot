f6ef8ab2d84dba45d457fda7f288ce02c0e7819e
Stefan Mansson <st.mansson@icloud.com>
2018-10-31 16:59:29 +0200

    test: update Content container test

--- src/containers/tests/Content.test.tsx
+++ src/containers/tests/Content.test.tsx
@@ -4,20 +4,23 @@ import Content from '../Content';
 import { SessionStatus } from '../../types';
 
 describe('Content', function() {
+    const MountedContent = mount(<Content />);
+
     it('should have a stopped session status', function() {
-        expect(shallow(<Content />).state('status')).toBe(SessionStatus.stopped);
+        expect(MountedContent.state('status')).toBe(SessionStatus.stopped);
     });
 
     it('should have no screenshots', function() {
-        expect((shallow(<Content />).state('screenshots') as []).length).toBe(0);
+        expect((MountedContent.state('screenshots') as []).length).toBe(0);
     });
 
-    it('should have a paragraph', function() {
-        expect(mount(<Content />).find('p').length).toBe(1);
+    it.skip('should update recording state', function() {
+        (MountedContent.instance() as Content).setRecordingState(SessionStatus.started);
+
+        expect(MountedContent.state('status')).toBe(SessionStatus.started);
     });
 
     it('pass snapshot', function() {
-        const tree = shallow(<Content />);
-        expect(tree).toMatchSnapshot();
+        expect(MountedContent).toMatchSnapshot();
     });
 });
