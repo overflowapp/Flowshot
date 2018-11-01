6da529c9ae214bd306f62720493a3239496629a7
Stefan Mansson <st.mansson@icloud.com>
2018-11-01 10:52:04 +0200

    test: chrome tests added

--- src/containers/tests/Content.test.tsx
+++ src/containers/tests/Content.test.tsx
@@ -3,14 +3,12 @@ import { shallow, mount } from 'enzyme';
 import Content from '../Content';
 import { SessionStatus } from '../../types';
 import { SessionStarted, SessionStopped, SessionDiscarded } from '../../components';
+import * as chrome from 'sinon-chrome';
 
 describe('Content', function() {
     const MountedContent = mount(<Content />);
 
     it('should have a stopped session status', function() {
-        const sessionStateProps = {
-            setRecordingState: (MountedContent.instance() as Content).setRecordingState,
-        };
         expect(MountedContent.state('status')).toBe(SessionStatus.stopped);
         expect(MountedContent.find(SessionStopped).length).toBe(1);
     });
@@ -30,9 +28,24 @@ describe('Content', function() {
         expect((MountedContent.state('screenshots') as []).length).toBe(0);
     });
 
+    it('should pull state from local storage', function() {
+        chrome.storage.local.get.withArgs(['recordingState']).yields({ recordingState: SessionStatus.started });
+
+        (MountedContent.instance() as Content).setFromLocalState();
+        expect(MountedContent.state('status')).toBe(SessionStatus.started);
+    });
+
+    it('should not set state if local storage empty', function() {
+        const initialStatus = MountedContent.state('status');
+
+        chrome.storage.local.get.withArgs(['recordingState']).yields({ recordingState: undefined });
+
+        (MountedContent.instance() as Content).setFromLocalState();
+        expect(MountedContent.state('status')).toBe(initialStatus);
+    });
+
     it.skip('should update recording state', function() {
         (MountedContent.instance() as Content).setRecordingState(SessionStatus.started);
-
         expect(MountedContent.state('status')).toBe(SessionStatus.started);
     });
 
--- src/setupEnzyme.ts
+++ src/setupEnzyme.ts
@@ -2,6 +2,14 @@ import { configure } from 'enzyme';
 import * as EnzymeAdapter from 'enzyme-adapter-react-16';
 import * as chrome from 'sinon-chrome/extensions';
 
+declare global {
+    namespace NodeJS {
+        interface Global {
+            chrome: chrome;
+        }
+    }
+}
+
 configure({ adapter: new EnzymeAdapter() });
 
-(global as any).chrome = chrome;
+global.chrome = chrome;
