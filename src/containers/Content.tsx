7eb5f8403a1fd37228be3d3621d254fba696b927
Stefan Mansson <st.mansson@icloud.com>
2018-11-01 10:13:16 +0200

    refactor: split session status content, add tests for them

--- src/containers/Content.tsx
+++ src/containers/Content.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { SessionStatus, Screenshot, RequestType } from '../types';
 import TabHelper from '../utils/TabHelper';
-import { Actions, Description } from '../components';
+import { SessionStarted, SessionDiscarded, SessionStopped } from '../components';
 
 interface AppProps {}
 
@@ -22,13 +22,17 @@ export default class Content extends React.Component<AppProps, AppState> {
 
     componentDidMount() {
         chrome.runtime.sendMessage({ popupMounted: true });
+        this.setFromLocalState();
+    }
+
+    setFromLocalState = () => {
         chrome.storage.local.get(['recordingState'], result => {
             result.recordingState &&
                 this.setState({
                     status: result.recordingState as SessionStatus,
                 });
         });
-    }
+    };
 
     listen() {
         chrome.runtime.onMessage.addListener((request, sender, respond) => {
@@ -66,66 +70,24 @@ export default class Content extends React.Component<AppProps, AppState> {
         });
     };
 
-    PopupContent = ({ sessionStatus }: { sessionStatus: SessionStatus }) => {
-        switch (true) {
-            case sessionStatus === SessionStatus.started:
-                return (
-                    <React.Fragment>
-                        <Description
-                            title="Recording..."
-                            titleClass="red@text"
-                            content="Your session is now being recorded. Clicks and websites visited are now tracked until the end of the session."
-                        />
-                        <Actions>
-                            <a className="button red flex-grow" onClick={() => this.setRecordingState(SessionStatus.discarded)}>
-                                Discard
-                            </a>
-                            <a className="ml4 button black flex-grow" onClick={() => this.setRecordingState(SessionStatus.stopped)}>
-                                Build your flow...
-                            </a>
-                        </Actions>
-                    </React.Fragment>
-                );
-                break;
-            case sessionStatus === SessionStatus.stopped:
-                return (
-                    <React.Fragment>
-                        <Description
-                            title="Let's get started."
-                            content="Start off your session and continue browsing the web. When done, end your session and download your Overflow file."
-                        />
-                        <Actions>
-                            <a className="button green flex-grow" onClick={() => this.setRecordingState(SessionStatus.started)}>
-                                Begin new session
-                            </a>
-                        </Actions>
-                    </React.Fragment>
-                );
-                break;
-            case sessionStatus === SessionStatus.discarded:
-                return (
-                    <React.Fragment>
-                        <Description
-                            title="Session discarded."
-                            content="The session has been discarded. Click &amp; website tracked has stopped until a new session has been initiated."
-                        />
-                        <Actions>
-                            <a className="button black flex-grow" onClick={() => this.setRecordingState(SessionStatus.stopped)}>
-                                Start Over
-                            </a>
-                        </Actions>
-                    </React.Fragment>
-                );
-                break;
+    PopupContent = () => {
+        switch (this.state.status) {
+            case SessionStatus.started:
+                return <SessionStarted setRecordingState={this.setRecordingState} />;
+            case SessionStatus.stopped:
+                return <SessionStopped setRecordingState={this.setRecordingState} />;
+            case SessionStatus.discarded:
+                return <SessionDiscarded setRecordingState={this.setRecordingState} />;
         }
     };
 
     render() {
-        const { screenshots, status } = this.state;
+        const { screenshots } = this.state;
+        const SessionState = this.PopupContent;
 
         return (
             <React.Fragment>
-                <this.PopupContent sessionStatus={status} />
+                <SessionState />
                 {screenshots.map(s => (
                     <img key={s.date} height={140} src={s.dataURI} />
                 ))}
--- src/containers/tests/Content.test.tsx
+++ src/containers/tests/Content.test.tsx
@@ -2,12 +2,28 @@ import * as React from 'react';
 import { shallow, mount } from 'enzyme';
 import Content from '../Content';
 import { SessionStatus } from '../../types';
+import { SessionStarted, SessionStopped, SessionDiscarded } from '../../components';
 
 describe('Content', function() {
     const MountedContent = mount(<Content />);
 
     it('should have a stopped session status', function() {
+        const sessionStateProps = {
+            setRecordingState: (MountedContent.instance() as Content).setRecordingState,
+        };
         expect(MountedContent.state('status')).toBe(SessionStatus.stopped);
+        expect(MountedContent.find(SessionStopped).length).toBe(1);
+    });
+
+    it('should render single component dependent on status', function() {
+        MountedContent.setState({ status: SessionStatus.started });
+        expect(MountedContent.find(SessionStarted).length).toBe(1);
+
+        MountedContent.setState({ status: SessionStatus.stopped });
+        expect(MountedContent.find(SessionStopped).length).toBe(1);
+
+        MountedContent.setState({ status: SessionStatus.discarded });
+        expect(MountedContent.find(SessionDiscarded).length).toBe(1);
     });
 
     it('should have no screenshots', function() {
