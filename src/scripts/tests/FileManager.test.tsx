52449599d36488c7e7f084b77ab42c8b80a442b9
Stefan Mansson <st.mansson@icloud.com>
2018-11-07 15:04:42 +0200

    test: cuid id test for FileManager

--- src/scripts/tests/FileManager.test.tsx
+++ src/scripts/tests/FileManager.test.tsx
@@ -1,21 +1,12 @@
-import * as chrome from 'sinon-chrome';
 import * as cuid from 'cuid';
 import FileManager from '../FileManager';
 import Flow from '../../../Flow/src/typings/Flow';
 
 describe('File Manager', function() {
-    it('should have a slug cuid title', function() {
-        const s1 = FileManager.structure;
+    it('should have a cuid title', function() {
+        const f1 = FileManager.flowStructure;
 
-        expect(cuid.isSlug(s1.title.split('-').pop())).toEqual(true);
-    });
-
-    it('should retrieve a unique structure each time', function() {
-        const s1 = FileManager.structure;
-        const s2 = FileManager.structure;
-
-        expect(s1.title).not.toEqual(s2.title);
-        expect(s1.date).toEqual(s2.date);
+        expect(cuid.isCuid(f1.document.id)).toEqual(true);
     });
 
     it('should get a Flow type structure', function() {
